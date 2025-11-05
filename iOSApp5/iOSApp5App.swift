b88b6d1c1676ddd79da838321a5c56d698119fab
Abdul <abdulaziz_safi@yahoo.com>
2025-11-05 08:11:15 -0500

    chore: scaffold iOSApp5 project

---
+++ iOSApp5/iOSApp5App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp5App.swift
+//  iOSApp5
+//
+//  Created by Abdul Aziz Safai on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSApp5Tests/iOSApp5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp5Tests.swift
+//  iOSApp5Tests
+//
+//  Created by Abdul Aziz Safai on 2025-11-05.
+//
+
+import Testing
+@testable import iOSApp5
+
+struct iOSApp5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
